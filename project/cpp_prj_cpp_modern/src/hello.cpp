f70bf82dba3bb5f227072e48f42a7c85103b06fe
yuwenxian <yuwenxian@ztgame.com>
2019-09-05 16:54:52 +0800

    update

---
+++ project/cpp_prj_cpp_modern/src/hello.cpp
@@ -0,0 +1,18 @@
+
+#include "common.h"
+#include "t_log.h"
+
+using namespace std;
+
+void t_test() 
+{
+}
+
+int main()
+{	
+	cout << "hello!" << endl;
+	//t_test();
+    t_log();
+
+	return 0;
+}
---
+++ project/cpp_prj_cpp_modern/src/t_log.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "common.h"
+
+using namespace std;
+
+void t_log()
+{
+    cout << __FUNCTION__ << endl;
+}
