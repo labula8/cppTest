d3dd1e67f598c2d2c879f6e92acf8e54bb6749eb
yuwenxian <yuwenxian@ztgame.com>
2019-09-06 18:46:35 +0800

    update

--- project/cpp_prj_cpp_modern/src/t_for.h
+++ project/cpp_prj_cpp_modern/src/t_for.h
@@ -10,9 +10,7 @@ void t_for() {
 
     cout << "==========" << endl;
 
-#ifdef STD_CPP_17
-    for (auto elem : m1) {
+    for (auto elem : m1.vec) {
         cout << "elem=" << elem << endl;
     }
-#endif
 }
